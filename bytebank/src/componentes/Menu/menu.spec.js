c8109d07f58caed472dd00db7976a57eaef2d72b
bbeluco <beluco.bispo@hotmail.com>
2023-08-26 18:20:56 -0300

    Criacao testes baseados em snapshot e class

--- bytebank/src/componentes/Menu/menu.spec.js
+++ bytebank/src/componentes/Menu/menu.spec.js
@@ -1,20 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 import Menu from './index';
 
 test('Deve renderizar um link para a pagina inicial', () => {
-    render(<Menu />);
-    const linkParaPaginaInicial = screen.getByText('Inicial');
-    expect(linkParaPaginaInicial).toBeInTheDocument();
-})
+  render(<Menu />);
+  const linkParaPaginaInicial = screen.getByText('Inicial');
+  expect(linkParaPaginaInicial).toBeInTheDocument();
+});
 
 test('Deve renderizar varios links', () => {
-    render(<Menu />);
-    const links = screen.getAllByRole('link');
-    expect(links).toHaveLength(4);
-})
+  render(<Menu />);
+  const links = screen.getAllByRole('link');
+  expect(links).toHaveLength(4);
+});
 
 test('Não deve renderizar o link para extrato', () => {
-    render(<Menu />);
-    const linkParaExtrato = screen.queryByText('Extrato');
-    expect(linkParaExtrato).not.toBeInTheDocument();
-})
+  render(<Menu />);
+  const linkParaExtrato = screen.queryByText('Extrato');
+  expect(linkParaExtrato).not.toBeInTheDocument();
+});
+
+test('Deve renderizar o link com a classe link', () => {
+  render(<Menu />);
+  const links = screen.getAllByRole('link');
+  links.forEach((link) => expect(link).toHaveClass('link'));
+  expect(links).toMatchSnapshot();
+});
